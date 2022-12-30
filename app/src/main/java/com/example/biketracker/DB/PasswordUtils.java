1bc5c34f78ee4dfdd97f2214ec1c006aea8ec610
Mark Harvey <mark990521@gmail.com>
2022-12-30 02:37:57 +0100

    Hashing, Registration Complete & currentLocation Created Password Hashing for Registration and Login Added CurrentLoction to Map
    30-12-2022

---
+++ app/src/main/java/com/example/biketracker/DB/PasswordUtils.java
@@ -0,0 +1,24 @@
+package com.example.biketracker.DB;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class PasswordUtils {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
